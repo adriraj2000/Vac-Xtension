a192e3c651f2bc892ba3502728200a2a2e9c9242
adriraj2000 <adrirajc@gmail.com>
2021-05-18 20:00:40 +0530

    using fetch instead of xhr

--- dist/background.js
+++ dist/background.js
@@ -1,4 +1,5 @@
-const api = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin";
+const api =
+  "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin";
 
 const getToday = () => {
   let today = new Date();
@@ -10,6 +11,31 @@ const getToday = () => {
   return today;
 };
 
+const sendHttpRequest = (method, url, data) => {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: data ? { "Content-Type": "application/json" } : {},
+  }).then((response) => {
+    if (response.status >= 400) {
+      // !response.ok
+      return response.json().then((errResData) => {
+        chrome.storage.sync.set(
+          {
+            last_run:
+              "There was an error in the last check. No slots found yet. Last run on: " +
+              new Date(),
+          },
+          function () {
+            console.log("Last run saved");
+          }
+        );
+      });
+    }
+    return response.json();
+  });
+};
+
 // searching slots by pin code
 // this function exists in the popup script too
 // need to find a way to make this modular
@@ -17,70 +43,52 @@ const getToday = () => {
 const searchForStock = (pin) => {
   const today = getToday();
   const params = "?pincode=" + pin + "&date=" + today;
-  try {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-        let responseText = xhr.response;
-        let response = JSON.parse(responseText);
-        response.centers.forEach((center) => {
-          center.sessions.forEach((session) => {
-            if (session.min_age_limit >= 18 && session.available_capacity > 0) {
-              // alert sound
-              var myAudio = new Audio(chrome.runtime.getURL("/notification.mp3"));
-              myAudio.play();
+  let url = api + params;
+  sendHttpRequest("GET", url).then((responseData) => {
+    responseData.centers.forEach((center) => {
+      center.sessions.forEach((session) => {
+        if (session.min_age_limit >= 18 && session.available_capacity > 0) {
+          var myAudio = new Audio(chrome.runtime.getURL("/notification.mp3"));
+          myAudio.play();
 
-              chrome.storage.sync.set({ last_found_slot: { center: center, session: session } });
-
-              //   alert and make a sound
-              if (
-                window.confirm(
-                  "A new slot was found!" +
-                    "\n" +
-                    center.name +
-                    " (" +
-                    session.available_capacity +
-                    ") " +
-                    " - on " +
-                    session.date +
-                    " - " +
-                    session.vaccine +
-                    " (" +
-                    center.fee_type +
-                    ")" +
-                    "\n" +
-                    "Click ok to open covin login page"
-                )
-              ) {
-                chrome.browserAction.onClicked.addListener(function (activeTab) {
-                  chrome.tabs.create({ url: 'https://selfregistration.cowin.gov.in/' });
-                });
-              }
-            }
+          chrome.storage.sync.set({
+            last_found_slot: { center: center, session: session },
           });
-        });
-      }
-    };
-    xhr.open("GET", api + params, true);
-    xhr.send(null);
+          if (
+            window.confirm(
+              "A new slot was found!" +
+                "\n" +
+                center.name +
+                " (" +
+                session.available_capacity +
+                ") " +
+                " - on " +
+                session.date +
+                " - " +
+                session.vaccine +
+                " (" +
+                center.fee_type +
+                ")" +
+                "\n" +
+                "Click ok to open covin login page"
+            )
+          ) {
+            chrome.browserAction.onClicked.addListener(function (activeTab) {
+              chrome.tabs.create({
+                url: "https://selfregistration.cowin.gov.in/",
+              });
+            });
+          }
+        }
+      });
+    });
     chrome.storage.sync.set(
       { last_run: "No slots found yet. Last run on: " + new Date() },
       function () {
         console.log("Last run saved");
       }
     );
-  } catch (error) {
-    console.log(error);
-    chrome.storage.sync.set(
-      {
-        last_run:
-          "There was an error in the last check. No slots found yet. Last run on: " + new Date(),
-      },
-      function () {
-        console.log("Last run saved");
-      }
-    );
-  }
+  });
 };
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
